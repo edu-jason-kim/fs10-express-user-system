c4792896cae0326bfe92ee2a608b88b364e53d60
edu-jason-kim <edu.jason.kim@gmail.com>
2025-12-30 16:19:18 +0900

    initial commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import express from "express";
+import productController from "./controllers/productController.js";
+import reviewController from "./controllers/reviewController.js";
+import userController from "./controllers/userController.js";
+import errorHandler from "./middlewares/errorHandler.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("", userController);
+app.use("/products", productController);
+app.use("/reviews", reviewController);
+
+app.use(errorHandler);
+
+const port = process.env.PORT ?? 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
